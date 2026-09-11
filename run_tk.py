7603215443ec24910fb29e2428ffa19bbb1c1800
gitlikang <mr.likang@qq.com>
2026-09-11 10:33:54 +0800

    fix(run_tk): 避免在无stdout时触发编码配置错误
    修复Windows平台下当标准输出不存在时调用reconfigure导致的异常

--- run_tk.py
+++ run_tk.py
@@ -23,7 +23,8 @@ import datetime
 # Fix console encoding for Windows
 import os
 os.system('chcp 65001 >nul')
-sys.stdout.reconfigure(encoding='utf-8')
+if sys.stdout is not None:
+    sys.stdout.reconfigure(encoding='utf-8')
 
 def _early_log(msg, tag="SYS"):
     try:
